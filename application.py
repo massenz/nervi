797339edda4f2fe1a9ec0d0c297fb857e8dd7712
Marco Massenzio <m.massenzio@gmail.com>
2014-06-20 17:16:50 -0700

    Minimalist form to try bs

--- application.py
+++ application.py
@@ -90,7 +90,7 @@ def build_fname(migration_id, ext):
 # Views
 @application.route('/')
 def home():
-    return render_template('index.html', workdir=get_workdir())
+    return render_template('index.html', workdir='foobar')
 
 @application.route('/api/v1/<migration_id>', methods=['POST'])
 def upload_data(migration_id):
